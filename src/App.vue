ee4b9313e8f0de5bbe7f390dfef0888facc97218
yuosif <yuosifraed@gmail.com>
2023-12-12 03:04:47 +0300

    Create new

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="app">
+  Hello ,hyrule
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 });
 </script>
@@ -20,8 +21,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #0d129d;
 }
 </style>
