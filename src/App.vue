3375f86896d5015f199be8c668e74e8dc503ca90
yuosif <yuosifraed@gmail.com>
2023-12-12 12:25:09 +0300

    vue component used with typeScript

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
 <div class="app">
-  Hello ,hyrule
+  <p>{{ name }} - {{  age }}</p>
+  <button @click="changeName('yoyo')">Change name</button>
+  <button @click="changeAge('30')">Change age</button>
 </div>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from 'vue';
 
+// let age :string|number
 
 export default defineComponent({
   name: 'App',
-  components: {
-    
+  components: { },
+  data(){
+    return {
+      name:'ali',
+      age:52 as number|string
+    }
+  },
+  methods:{
+    changeName(name:string)
+    {
+      this.name=name;
+    },
+    changeAge(age : number|string){
+       this.age=age;
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #0d129d;
-}
+
 </style>
